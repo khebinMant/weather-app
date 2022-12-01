b362c1ad26b973f513fedda2f7203737fca1fa4d
khabLexander <mantillagka@hotmail.com>
2022-12-01 13:54:18 -0500

    try

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { WeatherAppPage } from './pages/WeatherAppPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <WeatherAppPage />
-  // </React.StrictMode>
+  </React.StrictMode>
 );
